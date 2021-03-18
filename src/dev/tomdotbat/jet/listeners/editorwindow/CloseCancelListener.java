7bc6af6f7537d0e06d85e72dde3e3b4e7b829d5b
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-18 09:55:29 +0000

    Update CloseCancelListener.java

---
+++ src/dev/tomdotbat/jet/listeners/editorwindow/CloseCancelListener.java
@@ -0,0 +1,18 @@
+package dev.tomdotbat.jet.listeners.editorwindow;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CloseCancelListener implements ActionListener {
+    public CloseCancelListener(JDialog popup) { //Constructs the window close cancelled listener
+        this.popup = popup;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        popup.dispose(); //Close the popup
+    }
+
+    private JDialog popup;
+}
