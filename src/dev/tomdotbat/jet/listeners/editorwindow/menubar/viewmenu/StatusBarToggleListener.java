1df1691802df6e24f1734bd8702321ef279c7e01
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-18 10:01:06 +0000

    Update StatusBarToggleListener.java

---
+++ src/dev/tomdotbat/jet/listeners/editorwindow/menubar/viewmenu/StatusBarToggleListener.java
@@ -0,0 +1,22 @@
+package dev.tomdotbat.jet.listeners.editorwindow.menubar.viewmenu;
+
+import dev.tomdotbat.jet.windows.EditorWindow;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class StatusBarToggleListener implements ItemListener {
+    public StatusBarToggleListener(EditorWindow window) { //Constructs the toolbar toggle listener and attaches the editor window
+        this.window = window;
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        boolean state = e.getStateChange() == ItemEvent.SELECTED; //Is the toggle on or off?
+
+        window.getStatusBar().setVisible(state);
+        //set preference here
+    }
+
+    private final EditorWindow window;
+}
